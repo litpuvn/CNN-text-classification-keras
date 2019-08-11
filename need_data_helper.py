ec19904b19a22990ae456bd80f2c2d7277165955
Hoang Long <litpuvn@gmail.com>
2019-08-11 15:37:15 -0500

    correct spelling

--- need_data_helper.py
+++ need_data_helper.py
@@ -3,7 +3,7 @@ from data_helpers import build_vocab
 from data_helpers import build_input_data
 
 
-def lead_need_and_labels():
+def load_need_and_labels():
     return None, 1
 
 def load_need_data():
@@ -12,7 +12,7 @@ def load_need_data():
     Returns input vectors, labels, vocabulary, and inverse vocabulary.
     """
     # Load and preprocess data
-    sentences, labels = lead_need_and_labels()
+    sentences, labels = load_need_and_labels()
     sentences_padded = pad_sentences(sentences)
     vocabulary, vocabulary_inv = build_vocab(sentences_padded)
     x, y = build_input_data(sentences_padded, labels, vocabulary)
